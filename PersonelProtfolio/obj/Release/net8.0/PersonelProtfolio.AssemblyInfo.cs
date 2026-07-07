af452f92ad23b97c81da75dcb2eac1986600fc66
waseem <wasem1122999@gmail.com>
2026-07-07 14:58:16 +0300

    Added some pages and integrated with api backend server 1) Login , 2) Project List pages

--- PersonelProtfolio/obj/Release/net8.0/PersonelProtfolio.AssemblyInfo.cs
+++ PersonelProtfolio/obj/Release/net8.0/PersonelProtfolio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PersonelProtfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11fb8edac51c12cbb3b9587d9fff0aedeec5c13f")]
 [assembly: System.Reflection.AssemblyProductAttribute("PersonelProtfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PersonelProtfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
